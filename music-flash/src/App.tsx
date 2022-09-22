cd05b47879e9acfa63c01c5ccacd15490a199084
cedricsutter <104528161+cedricsutter@users.noreply.github.com>
2022-09-22 13:10:36 +0200

    Creat and deploy react app

---
+++ music-flash/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Ich bin dä Joshi und ich bin en riese Lauch, weil ich afoch nix cha.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
