db3e5ef567b0bffb57b474d66f85b7625fb3b63c
cedricsutter <104528161+cedricsutter@users.noreply.github.com>
2022-10-24 16:48:06 +0200

    Implemented add Blog

--- music-flash/src/index.tsx
+++ music-flash/src/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import Application from './application';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
